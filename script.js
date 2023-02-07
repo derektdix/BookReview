d245138eb84a3b9ee180feaca23e4a05f47b37a3
Derek Dix <derektdix@gmail.com>
2023-02-07 11:43:40 -0600

    started to add functionality to library

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ class Book {
     }
 }
 
+document.getElementById('bookSubmit').addEventListener('click', addBookToLibrary)
+
 function addBookToLibrary(){
-    
+    let name = document.getElementById('bookName').value
+    let author = document.getElementById('bookAuthor').value
+    let pageLength = Number(document.getElementById('bookPageLength').value)
+    let read = document.getElementById('bookRead').value
+
+
+    const book = new Book(name, author, pageLength, 5, read)
+    myLibrary.push(book)
 }
