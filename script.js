bb7484fffca84228bce99c807dbb966903e2c19e
Derek Dix <derektdix@gmail.com>
2023-02-07 11:05:41 -0600

    added form for submitting a book

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = []
+
+class Book {
+    constructor(name, author, pageLength, rating, read){
+        this.name = name
+        this.author = author
+        this.pageLength = pageLength
+        this.rating = rating
+        this.read = read
+    }
+}
+
+function addBookToLibrary(){
+    
+}
